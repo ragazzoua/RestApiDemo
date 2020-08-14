cd7aa4921a8e1ccba329e528a2c299880acda672
Serhii <ragazzo@ukr.net>
2020-08-14 09:43:55 +0300

    Assertions on Json Response Body and Headers

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,7 @@
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 
 public class FirstTest {
@@ -34,7 +35,9 @@ public class FirstTest {
                 .then()
                 .log()
                 .all()
-                .statusCode(200);
+                .statusCode(200)
+                .body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.18 (Ubuntu)");
 
 
     }
