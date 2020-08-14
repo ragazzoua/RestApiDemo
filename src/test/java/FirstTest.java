07e8af88d24b892cb8165d18843d9365c97b6e60
Serhii <ragazzo@ukr.net>
2020-08-14 09:48:50 +0300

    move body to the another class

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,4 @@
+import files.Payload;
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.given;
@@ -9,27 +10,11 @@ public class FirstTest {
     public static void main(String[] args) {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        String body = "{\n" +
-                "  \"location\": {\n" +
-                "    \"lat\": -38.383494,\n" +
-                "    \"lng\": 33.427362\n" +
-                "  },\n" +
-                "  \"accuracy\": 50,\n" +
-                "  \"name\": \"Frontline house\",\n" +
-                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                "  \"address\": \"29, side layout, cohen 09\",\n" +
-                "  \"types\": [\n" +
-                "    \"shoe park\",\n" +
-                "    \"shop\"\n" +
-                "  ],\n" +
-                "  \"website\": \"http://google.com\",\n" +
-                "  \"language\": \"French-IN\"\n" +
-                "}";
 
         //given - all input details
         //when - submit the Api - resource and http method
         //then - validate the response
-        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(body)
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(Payload.addPlace())
                 .when()
                 .post("maps/api/place/add/json")
                 .then()
