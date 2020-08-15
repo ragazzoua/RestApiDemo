ed8442eef6a86b2f9c358ad740f8818a27335ce0
Serhii <ragazzo@ukr.net>
2020-08-15 11:53:14 +0300

    put and get added

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,8 @@
 import files.Payload;
 import io.restassured.RestAssured;
+import io.restassured.internal.common.assertion.Assertion;
 import io.restassured.path.json.JsonPath;
+import org.junit.Assert;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -15,7 +17,8 @@ public class FirstTest {
         //given - all input details
         //when - submit the Api - resource and http method
         //then - validate the response
-        String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(Payload.addPlace())
+        String response = given().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json").body(Payload.addPlace())
                 .when()
                 .post("maps/api/place/add/json")
                 .then()
@@ -31,6 +34,28 @@ public class FirstTest {
         JsonPath jsonPath = new JsonPath(response); //for parsing Json
 
         String placeId = jsonPath.getString("place_id");
+
+        //Update place
+        String newAddress = "70 Summer walk, USA";
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body("{\n" +
+                        "\"place_id\":\"" + placeId + "\",\n" +
+                        "\"address\":\"" + newAddress + "\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}")
+                .when().put("maps/api/place/update/json")
+                .then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+
+        //Get Place
+        String newResponse = given().log().all().queryParam("key", "qaclick123")
+                .queryParam("place_id", placeId)
+                .when()
+                .get("maps/api/place/get/json")
+                .then().assertThat().statusCode(200).extract().response().asString();
+
+        JsonPath js = new JsonPath(newResponse);
+        String address = js.getString("address");
+
     }
 
 
