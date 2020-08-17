f9e045d0b87cd84bd1b0b699718f87c1a96b26fb
Serhii <ragazzo@ukr.net>
2020-08-17 09:46:14 +0300

    get dashboard.purchaseAmount

--- src/test/java/files/ComplexJsonpath.java
+++ src/test/java/files/ComplexJsonpath.java
@@ -11,9 +11,9 @@ public class ComplexJsonpath {
         int anInt = jsonPath.getInt("courses.size()");
 
         System.out.println(anInt);
+
+        int anInt1 = jsonPath.getInt("dashboard.purchaseAmount");
+
+        System.out.println(anInt1);
     }
-
-
-
-
 }
