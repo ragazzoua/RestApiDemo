5ca8ab6d170c91ec308c7388edc3139a0445789a
Serhii <ragazzo@ukr.net>
2020-08-17 09:44:17 +0300

    get courses amount

--- src/test/java/files/ComplexJsonpath.java
+++ src/test/java/files/ComplexJsonpath.java
@@ -4,5 +4,16 @@ import io.restassured.path.json.JsonPath;
 
 public class ComplexJsonpath {
 
-    JsonPath jsonPath = new JsonPath(Payload.coursePrice());
+    public static void main(String[] args) {
+
+        JsonPath jsonPath = new JsonPath(Payload.coursePrice());
+
+        int anInt = jsonPath.getInt("courses.size()");
+
+        System.out.println(anInt);
+    }
+
+
+
+
 }
