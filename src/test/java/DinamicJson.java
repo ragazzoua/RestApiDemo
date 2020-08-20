ae8f7784fba6dfb2ed0bb995354445d6ede539e7
Serhii <ragazzo@ukr.net>
2020-08-20 22:32:29 +0300

    data provider added

--- src/test/java/DinamicJson.java
+++ src/test/java/DinamicJson.java
@@ -2,6 +2,7 @@ import files.Payload;
 import files.ReUsableMethods;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,7 +10,7 @@ import static io.restassured.RestAssured.given;
 public class DinamicJson {
 
 
-    @Test
+    @Test(dataProvider = "BooksData")
     public void addBook(){
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         String response = given().header("Content-Type", "application/json")
@@ -21,6 +22,12 @@ public class DinamicJson {
 
         JsonPath js = ReUsableMethods.rawToJson(response);
         System.out.println(js.get("ID").toString());
+    }
+
+    @DataProvider(name = "BooksData")
+    public Object[][] getData(){
+
+        return new Object[][]{{"2323","535345"},{"2342345","22ggs"},{"cgsfsd","rewrwer"}};
     }
 
 }
