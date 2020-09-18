951c57f5601356f90e5655c8a74613d62d56b1e8
Serhii <ragazzo@ukr.net>
2020-09-18 08:40:51 +0300

    Complete End to end Test case with Serialization

--- src/test/java/SerializeTest.java
+++ src/test/java/SerializeTest.java
@@ -1,5 +1,10 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import pojo.AddPlace;
+import pojo.Location;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -7,8 +12,24 @@ public class SerializeTest {
     public static void main(String[] args) {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
 
+        AddPlace addPlace = new AddPlace();
+        Location location = new Location();
+        List<String> types = new ArrayList<>();
+        addPlace.setAccuracy(50);
+        addPlace.setAddress("");
+        addPlace.setLanguage("");
+        addPlace.setPhone("");
+        addPlace.setWebsite("");
+        addPlace.setName("");
+        location.setLat(8686.22);
+        location.setLng(886.22);
+        addPlace.setLocation(location);
+        types.add("shop");
+        types.add("shop1");
+        addPlace.setTypes(types);
+
         Response response = given().queryParam("key", "qaclick123")
-                .body("")
+                .body(addPlace)
                 .when()
                 .post("maps/api/place/add/json")
                 .then()
